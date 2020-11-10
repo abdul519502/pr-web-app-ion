89ccaedb1f655568df9239adaf5de386dc55f299
abdul519502 <69996382+abdul519502@users.noreply.github.com>
2020-11-09 23:41:29 -0500

    updated with application2.war

--- build-customAMI.sh
+++ build-customAMI.sh
@@ -5,5 +5,5 @@ sudo apt-get update -y
 sudo apt-get upgrade -y
 sudo apt-get install tomcat8 tomcat8-docs tomcat8-examples tomcat8-admin -y
 cd /var/lib/tomcat8/webapps/
-sudo wget https://hyderabad2020.s3.us-east-2.amazonaws.com/application.war
+sudo wget https://hyderabad2020.s3.us-east-2.amazonaws.com/application2.war
 sudo systemctl start tomcat8
